a9d4cce8ea850ac3f85eb068bf38f27eb32a3311
gc-mahesh-kumar <maheshk@gcptrain.org>
2025-06-29 02:24:00 +0000

    Added Environment Logic

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "<h1>Hello World from Google Cloud - Cloud Build + Cloud Deploy Demostration with Approval and Deploy to Cloud Run</h1>"
+    name = os.getenv("NAME", "World")  # Default to "World" if NAME is not set
+    return f"<h1>Hello {name} from Google Cloud - Cloud Build + Cloud Deploy</h1>"
 
 @app.route("/version")
 def version():
-  return "<h2>Helloworld 1.0</h2>"
+    return "<h2>Helloworld 1.0</h2>"
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port=8080)
+    app.run(host='0.0.0.0', port=8080)
